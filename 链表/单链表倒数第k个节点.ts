5c6b8923ac9b7bc2a981e27105b4c62900635932
myjdml <898762316@qq.com>
2021-10-01 20:30:40 +0800

    链表倒数第n个节点

---
+++ 链表/单链表倒数第k个节点.ts
@@ -0,0 +1,26 @@
+/*
+ * @Author: myjdml
+ * @Date: 2021-10-01 18:55:30
+ * @LastEditors: myjdml
+ * @LastEditTime: 2021-10-01 20:27:14
+ * @Description: 寻找单链表倒数第k个节点
+ */
+
+import { arrayToListNode, ListNode } from "./utils"
+
+const findFromEnd = (head: ListNode, k: number): ListNode | null => {
+  let p1: ListNode | null = head.next
+  for (let i = 0; i < k; i++) {
+    p1 = p1!.next;
+  }
+  let p2: ListNode | null = head.next
+  while (p1 != null) {
+    p1 = p1!.next
+    p2 = p2!.next
+  }
+  return p2
+}
+
+const head: ListNode = arrayToListNode([1,2,3,4,5,6,7,8,9,10])
+
+console.log(findFromEnd(head, 2))
