88926a52a073291e360857ccead2b871ab8c595e
myjdml <898762316@qq.com>
2021-10-01 18:54:31 +0800

    完成合并k个升序链表

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import { MaxPriorityQueue } from "./utils/PriorityQueue";
+import { MaxPriorityQueue, MinPriorityQueue } from "./utils/PriorityQueue";
 
 /*
  * @Author: myjdml
  * @Date: 2021-09-06 17:30:43
  * @LastEditors: myjdml
- * @LastEditTime: 2021-09-30 14:50:50
+ * @LastEditTime: 2021-10-01 18:22:18
  * @Description: 
  */
 const querry = new MaxPriorityQueue(7);
@@ -14,6 +14,13 @@ querry.insert(6)
 querry.insert(34)
 querry.print()
 
+const min_querry = new MinPriorityQueue(7)
+min_querry.insert(34)
+min_querry.insert(6)
+min_querry.insert(4)
+min_querry.insert(3)
+min_querry.insert(2)
+min_querry.print()
 
 
 
--- 链表/合并k个升序链表.ts
+++ 链表/合并k个升序链表.ts
@@ -2,30 +2,63 @@
  * @Author: myjdml
  * @Date: 2021-09-28 10:27:46
  * @LastEditors: myjdml
- * @LastEditTime: 2021-09-30 01:52:53
+ * @LastEditTime: 2021-10-01 18:52:25
  * @Description: 
  */
-import { MaxPriorityQueue } from "../utils/PriorityQueue";
+import { MaxPriorityQueue, MinPriorityQueue } from "../utils/PriorityQueue";
 import { logListNode, ListNode, arrayToListNode } from "./utils";
 
 const mergeKLists = (Lists: number[][]): ListNode | null => {
   if (Lists.length === 0) {
     return null
   }
+  // 将二维数组转换为链表数组
+  const Lists_nodeList: ListNode[] = new Array(Lists.length)
+  Lists.forEach((item, index) => {
+    Lists_nodeList[index] = arrayToListNode(item)
+  })
+  console.log(Lists_nodeList);
+  
+  // 虚拟头结点
   const dummy: ListNode = new ListNode(-1)
-  let p: ListNode = dummy
-
+  // 节点指针
+  let p: ListNode | null = dummy
+  let total_len: number = 0
+  Lists.forEach((item) => {
+    total_len += item.length
+  })
+  // 将所有元素填入链表
+  const pq = new MinPriorityQueue(total_len)
+  Lists_nodeList.forEach(item => {
+    let p: ListNode | null = item.next
+    while (p != null) {
+      pq.insert(p.val)
+      p = p.next
+    }
+  })
+  pq.print()
+  
+  while (!pq.isEmpty()) {
+    p.next = new ListNode(pq.delMin()!)
+    p = p.next
+    // pq.print()
+  }
+  
+  // logListNode(dummy)
+  
   return dummy
 }
 
-const querry = new MaxPriorityQueue(8)
-querry.print()
-querry.insert(3)
-querry.insert(8)
-querry.insert(4)
-querry.insert(6)
-querry.insert(5)
+mergeKLists([[1,2,3], [3,4,5], [23,66,22,1]])
 
-querry.print()
-querry.delMax()
-querry.print()
+// const querry = new MaxPriorityQueue(8)
+// querry.print()
+// querry.insert(3)
+// querry.insert(8)
+// querry.insert(4)
+// querry.insert(6)
+// querry.insert(5)
+
+// querry.print()
+// querry.delMax()
+// querry.print()
